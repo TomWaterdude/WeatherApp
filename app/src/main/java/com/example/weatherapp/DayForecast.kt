e6631f09a6db27ae6957def03907f26ad176eb6a
Tom Waterman <93015416+TomWaterdude@users.noreply.github.com>
2022-09-28 20:09:29 -0500

    assignment 3 finished

---
+++ app/src/main/java/com/example/weatherapp/DayForecast.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DayForecast(
+    var date: Long,
+    val sunrise: Long,
+    val sunset: Long,
+    val temp: ForecastTemp,
+    val pressure: Float,
+    val humidity: Int,
+): Parcelable
+
